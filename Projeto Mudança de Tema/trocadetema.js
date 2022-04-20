347538bb88c083fbb21a1ace460505aa4aa84172
HenriSousa <97175426+HenriSousa@users.noreply.github.com>
2022-04-20 11:02:59 -0300

    Update and rename Manipulando o DOM.js to trocadetema.js

--- Projeto Mudança de Tema/trocadetema.js
+++ Projeto Mudança de Tema/trocadetema.js
@@ -13,19 +13,19 @@ function changeClasses() {
 function changeText(){
     const ligthMode = 'Ligth Mode';
     const darkMode = 'Dark Mode';
-if(body.classList.contains(darkModeClass)){
-    button.innerHTML = ligthMode;
-    h1.innerHTML = darkMode + ' ON';
-    return;
+        if(body.classList.contains(darkModeClass)){
+            button.innerHTML = ligthMode;
+            h1.innerHTML = darkMode + ' ON';
+            return;
 }
-button.innerHTML = darkMode;
-h1.innerHTML = ligthMode + ' ON';
+            button.innerHTML = darkMode;
+            h1.innerHTML = ligthMode + ' ON';
+}
 
-}
 const darkModeClass = 'dark-mode';
 const button = document.getElementById('mode-selector');
-const h1 = document.getElementById('page-title');
+const h1 = document.getElementById('title');
 const body = document.getElementsByTagName('body')[0];
-const footer = document.getElementsByTagName('footer')[0];
+const footer = document.getElementsByClassName('RodaPe')[0];
 
 button.addEventListener('click', changeMode);
